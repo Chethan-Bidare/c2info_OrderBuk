950f567deb3bf2d7d9627386d4b8ea6e04e63b1c
chetan <chetan.gmail.com>
2017-12-29 14:58:42 +0530

    3rd commit

--- src/main/java/c2info_OrderBuk_TestBase/TestBase.java
+++ src/main/java/c2info_OrderBuk_TestBase/TestBase.java
@@ -1,5 +1,48 @@
 package c2info_OrderBuk_TestBase;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+
 public class TestBase {
+	
+	public static final Logger log = Logger.getLogger(TestBase.class.getName());
+	public WebDriver driver ;
+	public static ExtentReports extent ;
+	public static ExtentTest Test ;
+	public static ITestResult Result ;
+	public Properties OR = new Properties();
+	public Properties APP = new Properties();
+	
+	
+	public void LoadfromORproperties() throws IOException{
+		File path = new File(System.getProperty("user.dir")+"//src//main//java//c2info//OrderBuk//Config//OR.properties");
+		FileInputStream fis = new FileInputStream(path);
+		OR.load(fis);
+	}
+	
+	public void LoadfromAPPproperties() throws IOException{
+		File path = new File(System.getProperty("user.dir")+"//src//main//java//c2info//OrderBuk//Config//APP.properties");
+		FileInputStream fis = new FileInputStream(path);
+		APP.load(fis);
+	}
+	
+	static{
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat formatter = new SimpleDateFormat();
+		extent = new ExtentReports(System.getProperty("user.dir")+"//src//main//java//c2info//OrderBuk//Reports"+formatter.format(calendar.getTime())+".html",false);
+	}
+	
+	
 
 }

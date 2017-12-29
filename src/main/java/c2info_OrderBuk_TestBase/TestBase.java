1bac4697cb832697946227db08439a000c76acce
chetan <chetan.gmail.com>
2017-12-29 17:13:55 +0530

    4th commit

--- src/main/java/c2info_OrderBuk_TestBase/TestBase.java
+++ src/main/java/c2info_OrderBuk_TestBase/TestBase.java
@@ -8,11 +8,16 @@ import java.util.Calendar;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+
+import c2info_OrderBuk_ExcelReader.ExcelReader;
 
 public class TestBase {
 	
@@ -41,6 +46,44 @@ public class TestBase {
 		Calendar calendar = Calendar.getInstance();
 		SimpleDateFormat formatter = new SimpleDateFormat();
 		extent = new ExtentReports(System.getProperty("user.dir")+"//src//main//java//c2info//OrderBuk//Reports"+formatter.format(calendar.getTime())+".html",false);
+	}
+	
+	public String[][] ReadExcel(String SheetName,String ExcelName){
+		String path = System.getProperty("user.dir")+"//src//main//java//c2info//OrderBuk//Data//TestData.xlsx";
+		ExcelReader excel = new ExcelReader(path);
+		String[][] TestData = excel.getdatafromSheet(SheetName, ExcelName);
+		return TestData;
+		
+	}
+	
+	public void init() throws IOException{
+		LoadfromORproperties();
+		LoadfromAPPproperties();
+		SelectBrowser(OR.getProperty("BrowserName"));
+		GetBaseUrl(OR.getProperty("URL"));
+		PropertyConfigurator.configure(System.getProperty("user.dir")+"//log4j.properties");
+		
+	}
+	
+	public void SelectBrowser(String BrowserName){
+		if(BrowserName.equalsIgnoreCase("firefox")){
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//Drivers//geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		else if(BrowserName.equalsIgnoreCase("chrome")){
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+	}
+	
+	public void GetBaseUrl(String BaseUrl){
+		driver.get(BaseUrl);
+		try {
+			driver.manage().window().maximize();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	

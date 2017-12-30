0c29d587d8dc9aeaeaf310bf93055ea47f24f368
chetan <chetan.gmail.com>
2017-12-30 12:50:14 +0530

    changes commit

--- src/main/java/c2info_OrderBuk_UIPages/LoginPage.java
+++ src/main/java/c2info_OrderBuk_UIPages/LoginPage.java
@@ -1,5 +1,39 @@
 package c2info_OrderBuk_UIPages;
 
-public class LoginPage {
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import c2info_OrderBuk_TestBase.TestBase;
+
+public class LoginPage extends TestBase{
+
+	public static final Logger log = Logger.getLogger(LoginPage.class.getName());
+	
+	@FindBy(id="loginform-username")
+	WebElement UserName ;
+	
+	@FindBy(id="loginform-password")
+	WebElement Password ;
+	
+	@FindBy(name="login-button")
+	WebElement LoginBtn ;
+	
+	public LoginPage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void doLogin(String UserName,String pwd){
+		this.UserName.clear();
+		log.info("Clearing the Username field");
+		this.UserName.sendKeys(UserName);
+		log.info("Entering the Username :"+UserName);
+		Password.clear();
+		log.info("Clearing the Password field");
+		Password.sendKeys(pwd);
+		log.info("Entering the Password :"+pwd);
+		LoginBtn.click();
+		log.info("Clicked on Login button");
+	}
 }

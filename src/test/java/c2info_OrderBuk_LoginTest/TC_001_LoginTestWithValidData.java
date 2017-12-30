ecfe91bcdf005eba39e78f8fe05270e273c14590
chetan <chetan.gmail.com>
2017-12-30 15:49:35 +0530

    changes commit

---
+++ src/test/java/c2info_OrderBuk_LoginTest/TC_001_LoginTestWithValidData.java
@@ -0,0 +1,35 @@
+package c2info_OrderBuk_LoginTest;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import c2info_OrderBuk_TestBase.TestBase;
+import c2info_OrderBuk_UIPages.LoginPage;
+
+public class TC_001_LoginTestWithValidData extends TestBase {
+
+	public static final Logger log = Logger.getLogger(TC_001_LoginTestWithValidData.class.getName());
+
+	@BeforeClass
+	public void setup() throws IOException{
+		init();
+		log.info("Initializing Setup");
+		
+	}
+	
+	@Test
+	public void loginWithValidData(){
+		log.info("======= TC_001_LoginTestWithValidData Test Started ======");
+		LoginPage loginpage = new LoginPage();
+		loginpage.doLogin(OR.getProperty("UserName"), OR.getProperty("Password"));
+		
+		Assert.assertEquals(getPageTitle(), APP.getProperty("DashboardPageTitle"));
+	}
+	
+	
+}
